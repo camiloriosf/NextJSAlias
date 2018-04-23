522a9b9faeefe6865c95af2e0140874b993a7b2e
camiloriosf <camilo.rios.f@gmail.com>
2018-04-23 16:06:38 -0300

    change pathname to /api

--- client1/pages/index.js
+++ client1/pages/index.js
@@ -4,7 +4,7 @@ export default () => (
   <div>
     <p>This is the client 1 page</p>
     <div>
-      <Link href="/client2">
+      <Link href="/api/client2">
         <a>Client 2</a>
       </Link>
     </div>
